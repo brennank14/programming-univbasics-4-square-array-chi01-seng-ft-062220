e63d1e98fca3c31d199845dd8be1647e90df1f6b
Kathleen Brennan <brennank14@gmail.com>
2020-06-06 02:10:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-  numbers = [1, 2, 3]
   square_numbers = []
   array.length.times { |index|
     square_numbers.push((array[index])**2)
   }
   square_numbers
 end
+
+square_array[1, 2, 3]
